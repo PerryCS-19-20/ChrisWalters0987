f0864aca02161dbc0a3dcda904396c8f25cbb00d
ChrisWalters0987 <Cwalters0987@gmail.com>
2016-11-30 07:58:19 -0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,30 +1,33 @@
 function buildMap(ID)
 {
-    var Container = document.getElementById(ID);
+	var Container = document.getElementById(ID);
 
-    var NewUL = document.createElement("UL");
-    Container.appendChild(NewUL);
+	var NewUL = document.createElement("UL");
+	Container.appendChild(NewUL);
 
-    NewUL.appendChild(makeList("index.html", "Home"));
-    NewUL.appendChild(makeList("bio.html", "Biography"));
+	NewUL.appendChild(makeList("index.html", "Home"));
+	NewUL.appendChild(makeList("bio.html", "Biography"));
 
-    var NewLI = document.createElement("LI");
-    NewLI.textContent = "School Projects:";
-    NewUL.appendChild(NewLI);
+	var NewLI = document.createElement("LI");
+	NewLI.textContent = "School Projects:";
+	NewUL.appendChild(NewLI);
 
-    NewUL.appendChild(makeList("Breakout.html", "Breakout"));
-    NewUL.appendChild(makeList("Breakout.html", "JavaScript Events"));
-    NewUL.appendChild(makeList("Breakout.html", "Breakout"));
-    NewUL.appendChild(makeList("Breakout.html", "Breakout"));
-    NewUL.appendChild(makeList("Breakout.html", "Breakout"));
+	NewUL.appendChild(makeList("Breakout.html", "Breakout!"));
+	NewUL.appendChild(makeList(".html", "JavaScript Events!"));
+	NewUL.appendChild(makeList("popup.html", "Pop-Up!"));
+	NewUL.appendChild(makeList("todolist.html", "To Do List!"));
+	NewUL.appendChild(makeList("chalkbored.html", "Chalkbored!"));
+	NewUL.appendChild(makeList("tictactoe.html", "Tic-Tac-Toe!"));
+	NewUL.appendChild(makeList("FirstMultiplicationTable.html", "Multiplication!"));
+	NewUL.appendChild(makeList("BetterMutiTable.html", "More Multiplication!"));
 }
 
 function makeList(Link, Text)
 {
-    var NewA = document.createElement("a");
-    var NewLI = document.createElement("LI");
-    NewLI.appendChild(NewA);
-    NewA.textContent = Text;
-    NewA.href = Link;
-    return NewLI;
+	var NewA = document.createElement("a");
+	var NewLI = document.createElement("LI");
+	NewLI.appendChild(NewA);
+	NewA.textContent = Text;
+	NewA.href = Link;
+	return NewLI;
 }
