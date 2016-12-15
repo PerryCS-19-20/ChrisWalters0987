ce2160ba4d77377615024e2062f0d47b3d00438b
ChrisWalters0987 <Cwalters0987@gmail.com>
2016-12-15 09:36:32 -0700

    Add files via upload

---
+++ SiteMap/sitemap.js
@@ -0,0 +1,36 @@
+function buildMap(ID)
+{
+    var Container = document.getElementById(ID);
+
+    var NewUL = document.createElement("UL");
+    Container.appendChild(NewUL);
+    NewUL.appendChild(makeList("../Index.html", "Home"));
+    NewUL.appendChild(makeList("../Bio/bio.html", "Biography"));
+
+    var NewLI = document.createElement("LI");
+    NewLI.textContent = "School Projects:";
+    NewUL.appendChild(NewLI);
+
+    var NewNewUL = document.createElement("UL");
+    NewLI.appendChild(NewNewUL);
+
+    NewNewUL.appendChild(makeList("../Breakout.html", "Breakout!"));
+    NewNewUL.appendChild(makeList(".html", "JavaScript Events!"));
+    NewNewUL.appendChild(makeList("../popup.html", "Pop-Up!"));
+    NewNewUL.appendChild(makeList("../todolist.html", "To Do List!"));
+    NewNewUL.appendChild(makeList("../chalkbored.html", "Chalkbored!"));
+    NewNewUL.appendChild(makeList("../tictactoe/tictactoe.html", "Tic-Tac-Toe!"));
+    NewNewUL.appendChild(makeList("../FirstMultiplicationTable.html", "Multiplication!"));
+    NewNewUL.appendChild(makeList("../BetterMutiTable.html", "More Multiplication!"));
+    NewNewUL.appendChild(makeList("../Calculator/Calculator.html", "Midpoint Calculation!"));
+}
+
+function makeList(Link, Text)
+{
+    var NewA = document.createElement("a");
+    var NewLI = document.createElement("LI");
+    NewLI.appendChild(NewA);
+    NewA.textContent = Text;
+    NewA.href = Link;
+    return NewLI;
+}
